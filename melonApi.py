6f3f45f43c5cff8dffdb8cdb216fb7844d68187c
daniel <danielko2001@gmail.com>
2019-12-14 23:29:09 -0600

    test log

--- melonApi.py
+++ melonApi.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True)
-
+"""
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-
+"""
 @app.route('/', methods=['GET'])
 def spalsh():
     return "<h1> Melon API </h1> <p>Try calling /chart/(live, rise, day, week, month)</p>"
