34b197962ad053c4a9ead3e0f3e0113ad30e0326
daniel <danielko2001@gmail.com>
2019-12-14 23:32:43 -0600

    fix heroku issue

--- melonApi.py
+++ melonApi.py
@@ -1,20 +1,14 @@
 """
 Simple flask api 
 """
-import logging
-from flask import Flask, jsonify
+import flask
 from scrapeMelon import getList
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 if __name__ == '__main__':
     app.run(threaded=True, debug=True)
-"""
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-"""
+
 @app.route('/', methods=['GET'])
 def spalsh():
     return "<h1> Melon API </h1> <p>Try calling /chart/(live, rise, day, week, month)</p>"
