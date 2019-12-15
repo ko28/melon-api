7d889cfade03294efb2dcf853cf191a773c9775a
daniel <danielko2001@gmail.com>
2019-12-14 23:06:46 -0600

    heroku test

--- melonApi.py
+++ melonApi.py
@@ -7,12 +7,14 @@ from scrapeMelon import getList
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+if __name__ == '__main__':
+    app.run(threaded=True)
+
 @app.route('/', methods=['GET'])
-def spalsh  ():
-    return "<h1> Melon API </h1>"
+def spalsh():
+    return "<h1> Melon API </h1> <p>Try calling /chart/(live, rise, day, week, month)</p>"
 
 @app.route('/chart/<string:key>', methods=['GET'])
 def chart(key):
     return getList(key.upper())
 
-app.run()
