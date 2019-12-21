298d47b47a386e355e1d56c18e08fc85511af2d1
daniel <ko28@wisc.edu>
2019-12-20 22:42:51 -0600

    trying to fix crash on heroku

--- melonApi.py
+++ melonApi.py
@@ -28,4 +28,7 @@ def makePlaylist():
 
 # Needs to be on the bottom 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(threaded=True, debug=True)
+    
