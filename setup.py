c3b30d54939f7afd129e5369893cccbbb4b42c06
daniel <ko28@wisc.edu>
2020-05-17 15:55:07 -0500

    rename

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="melonapi", 
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Daniel Ko",
+    author_email="danielhbko@gmailc.om",
+    description="A small Melon api",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ko28/melonApi/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
