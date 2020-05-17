d3113dd209272c47c8730dd7702b4e1d5d15a015
daniel <ko28@wisc.edu>
2020-05-17 16:17:09 -0500

    update setup2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="melonapi", 
-    version="0.0.1",
+    version="0.0.2",
     author="Daniel Ko",
     author_email="danielhbko@gmail.com",
     description="A small Melon api",
