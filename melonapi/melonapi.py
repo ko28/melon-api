5bee6ecff63ea9e7b47033886d5e661704fc5cf3
sardap <sardap@users.noreply.github.com>
2021-02-06 10:51:00 +1100

    added getting top chart for specific years

--- melonapi/melonapi.py
+++ melonapi/melonapi.py
@@ -5,51 +5,123 @@ import flask
 import os
 import requests
 import json
-from . import spotify
+from datetime import datetime
+from flask import request
+from re import match
+from werkzeug.routing import BaseConverter
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from .spotify import token as createToken
+from .spotify import makePlaylist
+from .spotify import convertChartToPlaylist
 from .scrapeMelon import getList
 from .scrapeMelon import getLyric
-from datetime import datetime
+from .scrapeMelon import yearChart
 
 app = flask.Flask(__name__)
 
-#Settings for playlist
+default_headers = {'Content-Type': 'text/json'}
+
+# Settings for playlist
 client_id = os.environ['client_id']
 client_secret = os.environ['client_secret']
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app.url_map.converters['regex'] = RegexConverter
+
 
 @app.route('/', methods=['GET'])
 def spalsh():
     return flask.render_template('index.html')
 
+
 @app.route('/chart/<string:key>', methods=['GET'])
 def chart(key):
-    return getList(key.upper())
+    return getList(key.upper()), 200, default_headers
+
+
+@app.route('/chart/year/<regex("(2[0-9]{3}|19[0-9]{2})"):year>', methods=['GET'])
+def listAge(year):
+    if int(year) < 1964 or int(year) >= datetime.now().year:
+        return {"message": "invalid year"}, 400, default_headers
+
+    overseas = request.args.get('overseas')
+
+    if overseas != None:
+        fileName = "overseas"
+    else:
+        fileName = "local"
+
+    fileName = os.path.join(
+        "static", "scrape_year", "{}_{}.json".format(fileName, year))
+
+    return flask.send_file(fileName, mimetype="text/json")
+
 
 @app.route('/lyric/<string:key>', methods=['GET'])
 def lyric(key):
-    return getLyric(key)
+    return getLyric(key), 200, default_headers
 
-#Buggy, removed for now
+
 @app.route('/spotify', methods=['GET'])
 def getOAuthCode():
     # Go to Spotify's authorization page to get authorization code
-    return flask.redirect('https://accounts.spotify.com/authorize?client_id=' + client_id + 
-    '&response_type=code&redirect_uri=' + flask.request.host_url + 'spotify/playlist&scope=playlist-modify-public')  
+    return flask.redirect('https://accounts.spotify.com/authorize?client_id=' + client_id +
+                          '&response_type=code&redirect_uri=' + flask.request.host_url + 'spotify/playlist&scope=playlist-modify-public')
 
- 
+
 @app.route('/spotify/playlist', methods=['GET'])
-def makePlaylist():  
-    liveChart = getList("LIVE")  
-    token = spotify.token(flask.request.args.get('code'), flask.request.host_url + 'spotify/playlist', client_id, client_secret)    
-    playlist = spotify.makePlaylist(token)
-    return "https://open.spotify.com/playlist/"+spotify.convertChartToPlaylist(liveChart, playlist['id'], token)
+def makePlaylist():
+    liveChart = getList("LIVE")
+    token = createToken(flask.request.args.get(
+        'code'), flask.request.host_url + 'spotify/playlist', client_id, client_secret)
+    playlist = makePlaylist(token)
+    return "https://open.spotify.com/playlist/"+convertChartToPlaylist(liveChart, playlist['id'], token)
+
+# Pre pull and render pages to get previous year charts
 
 
-'''
-    except:
-        return "Could not get token, try clearing your cookies and logging again."
-        '''
-   
-# Needs to be on the bottom 
+def scrapeYears():
+    if not os.path.isdir(os.path.join("static", "scrape_year")):
+        os.mkdir(os.path.join("static", "scrape_year"))
+
+    seleniumDriver = None
+
+    # Creating this driver is slow so we don't always want to do it
+    def getDriver():
+        nonlocal seleniumDriver
+        if seleniumDriver == None:
+            # Headless mode is causing parsing errors + Firefox is also
+            seleniumDriver = webdriver.Chrome()
+
+        return seleniumDriver
+
+    # 1964 is the furthest the charts go back
+    for year in range(1964, datetime.now().year):
+        localFile = os.path.join(
+            "static", "scrape_year", "local_{}.json".format(year))
+        overseasFile = os.path.join(
+            "static", "scrape_year", "overseas_{}.json".format(year))
+
+        if not os.path.isfile(localFile):
+            with open(localFile, "w", encoding="utf-8") as f:
+                f.write(json.dumps(
+                    yearChart(getDriver(), str(year), True), indent=4))
+
+        if not os.path.isfile(overseasFile):
+            with open(overseasFile, "w", encoding="utf-8") as f:
+                f.write(json.dumps(
+                    yearChart(getDriver(), str(year), False), indent=4))
+
+
+# Needs to be on the bottom
 if __name__ == '__main__':
     app.run(threaded=True, debug=True)
-    
+
+scrapeYears()
--- melonapi/scrapeMelon.py
+++ melonapi/scrapeMelon.py
@@ -1,7 +1,10 @@
 import requests
 import re
-from bs4 import BeautifulSoup
 import json
+import os
+import os
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 URL = {
     'LIVE': 'https://www.melon.com/chart/index.htm',
@@ -14,17 +17,15 @@ URL = {
 
 def getList(time):
     """Generates json file of the top 100 songs + (additional metadata) on Melon
-
     Args:
         time (str): Which chart you want (LIVE, RISE, DAY, WEEK, MONTH)
-
     Returns:
         json (str): Seralized json string that contains the top 100 songs. 
                     Key is ranking of song; value is name, ranking, artists, songId, albumId (id's are Melon specific).  
-					NOTE: You want to use json.loads(getList("time")) to deseralize the data. 
-
+                    NOTE: You want to use json.loads(getList("time")) to deseralize the data. 
     """
-    html = requests.get(URL[time.upper()], headers={'User-Agent':"github.com/ko28/melon-api"}).text
+    html = requests.get(URL[time.upper()], headers={
+                        'User-Agent': "github.com/ko28/melon-api"}).text
     soup = BeautifulSoup(html, "lxml")
     data = {}
     # Melon recently changed how their live chart works, tried to fix it as best as I could
@@ -39,7 +40,7 @@ def getList(time):
                 "songId": re.search(r'goSongDetail\(\'(.*)\'\)', str(tag)).group(1),
                 "albumId": re.search(r'goAlbumDetail\(\'(.*)\'\)', str(tag)).group(1)
             }
-            rank+=1
+            rank += 1
 
     else:
         for tag in soup.findAll("tr", {"class": ["lst50", "lst100"]}):
@@ -54,10 +55,54 @@ def getList(time):
         # Some data is in Korean, must format with utf-8 to avoid printing out utf code
     return json.dumps(data, ensure_ascii=False).encode('utf-8')
 
+
 def getLyric(songId):
     url = 'https://www.melon.com/song/detail.htm?songId='+str(songId)
-    req = requests.get(url, headers={'User-Agent':"github.com/ko28/melon-api"})
+    req = requests.get(
+        url, headers={'User-Agent': "github.com/ko28/melon-api"})
     html = req.text.replace("<BR>", "\n")
     soup = BeautifulSoup(html, "lxml")
     lyrics = soup.find("div", {"class": "lyric"})
-    return lyrics.text.strip() 
+    return lyrics.text.strip()
+
+# This is a bit of a mess since these pages are rendered via javascript we need selenium
+
+
+def yearChart(seleniumDriver, year, domestic=True):
+    """Generates json file of the top 50 songs + (additional metadata) on Melon
+
+    Args:
+        year (str): the year you wish to fetch
+        domestic (bool): weather to pull domestic (korean) song chart or overseas chart
+
+    Returns:
+        json (str): Seralized json string that contains the top 50 songs for year.
+                    Key is ranking of song; value is name, ranking, artists, songId, albumId (id's are Melon specific).
+                                        NOTE: You want to use json.loads(getList("time")) to deseralize the data.
+
+    """
+
+    chartGenere = "KPOP" if domestic else "POP"
+    url = "https://www.melon.com/chart/age/index.htm?chartType=YE&chartGenre={}&chartDate={}".format(
+        chartGenere, year)
+
+    seleniumDriver.get(url)
+    html = seleniumDriver.page_source
+
+    soup = BeautifulSoup(html, "lxml")
+    data = []
+
+    bottomList = soup.find("div", {"id": ["tb_list"]})
+
+    for tag in bottomList.findAll("tr", {"class": ["lst50"]}):
+        albumId = re.search(r'goAlbumDetail\(\'(.*)\'\)', str(tag)).group(1)
+        data.append({
+            "name": tag.find("div", {"class": "ellipsis rank01"}).getText().strip(),
+            "ranking": tag.find("span", {"class": ["rank top", "rank"]}).getText(),
+            "artists": tag.find("span", {"class": "checkEllipsis"}).getText(),
+            "songId": re.search(r'goSongDetail\(\'(.*)\'\)', str(tag)).group(1),
+            "albumId": albumId,
+            "albumLink": "https://www.melon.com/album/detail.htm?albumId={}".format(albumId)
+        })
+
+    return data
